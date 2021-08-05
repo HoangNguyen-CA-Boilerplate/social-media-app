b9b06167cd94241d7c6291091bcd7517fcc9ef64
Hoang <hoang.viet.nguyen@outlook.com>
2021-08-05 14:57:32 -0400

    added SignupForm.test.js (similar to LoginForm.test.js)

--- client/src/auth/__tests__/SignupForm.test.js
+++ client/src/auth/__tests__/SignupForm.test.js
@@ -1,10 +1,11 @@
 import { render, screen } from '../../test-utils';
-import LoginForm from '../LoginForm';
+import SignupForm from '../SignupForm';
 import userEvent from '@testing-library/user-event';
 import { waitFor } from '@testing-library/react';
 
 describe('auth/SignupForm', () => {
   let submitBtn;
+  let usernameInput;
   let emailInput;
   let passwordInput;
   let form;
@@ -12,8 +13,9 @@ describe('auth/SignupForm', () => {
 
   beforeEach(() => {
     mockSubmit = jest.fn();
-    render(<LoginForm onSubmit={mockSubmit} />);
+    render(<SignupForm onSubmit={mockSubmit} />);
     submitBtn = screen.getByRole('button', { name: /Submit/i });
+    usernameInput = screen.getByLabelText(/Username:/i);
     emailInput = screen.getByLabelText(/Email:/i);
     passwordInput = screen.getByLabelText(/Password:/i);
     form = screen.getByRole('form');
@@ -21,13 +23,15 @@ describe('auth/SignupForm', () => {
 
   it('Should display error if fields are empty', async () => {
     userEvent.click(submitBtn);
-    expect(await screen.findAllByRole('alert')).toHaveLength(2);
+    expect(await screen.findAllByRole('alert')).toHaveLength(3);
   });
 
   it('Should display error if email is invalid', async () => {
+    userEvent.type(usernameInput, 'bobob');
     userEvent.type(emailInput, 'bob');
     userEvent.type(passwordInput, 'bobob');
 
+    expect(usernameInput).toHaveValue('bobob');
     expect(emailInput).toHaveValue('bob');
     expect(passwordInput).toHaveValue('bobob');
 
@@ -39,10 +43,29 @@ describe('auth/SignupForm', () => {
     });
   });
 
+  it('Should display error if username is invalid', async () => {
+    userEvent.type(usernameInput, 'bo');
+    userEvent.type(emailInput, 'bob@gmail.com');
+    userEvent.type(passwordInput, 'bobob');
+
+    expect(usernameInput).toHaveValue('bo');
+    expect(emailInput).toHaveValue('bob@gmail.com');
+    expect(passwordInput).toHaveValue('bobob');
+
+    userEvent.click(submitBtn);
+
+    await waitFor(() => {
+      expect(mockSubmit).toHaveBeenCalledTimes(0);
+      expect(screen.getByRole('alert')).toHaveTextContent(/username/i);
+    });
+  });
+
   it('Should display error if password is invalid', async () => {
+    userEvent.type(usernameInput, 'bobob');
     userEvent.type(emailInput, 'bob@gmail.com');
     userEvent.type(passwordInput, 'bob');
 
+    expect(usernameInput).toHaveValue('bobob');
     expect(emailInput).toHaveValue('bob@gmail.com');
     expect(passwordInput).toHaveValue('bob');
 
@@ -56,9 +79,11 @@ describe('auth/SignupForm', () => {
   it('Should be submit form if fields are valid', async () => {
     form.onsubmit = null;
 
+    userEvent.type(usernameInput, 'bobob');
     userEvent.type(emailInput, 'bob@gmail.com');
     userEvent.type(passwordInput, 'bobob');
 
+    expect(usernameInput).toHaveValue('bobob');
     expect(emailInput).toHaveValue('bob@gmail.com');
     expect(passwordInput).toHaveValue('bobob');
 
